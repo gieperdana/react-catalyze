40f8ab9edab6f595d19c4e3db2a8283aa0e51be1
Muhamad Anggi Perdana <muh.anggi.perdana@gmail.com>
2021-03-08 12:04:11 +0700

    [FIX]Hover logic

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,52 @@ import React, { useState } from 'react';
 import {Link} from "react-router-dom";
 import {Header,Footer,Screen,MenuBar,Clients,SocialMedia} from "../component";
 const Home = () => {
-    const [hover, setHover] = useState(false);
-
+    const [hover, setHover] = useState({activeIndex: 0});
+    // handleClick = (index) => this.setHover({ activeIndex: index })
+    const articles = [
+        {
+            "id":1,
+            "image":"https://picsum.photos/900",
+            "title":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ut nunc quis justo soda",
+            "date":"22 Oct 2020",
+            "content":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident."
+        },
+        {
+            "id":2,
+            "image":"https://picsum.photos/900",
+            "title":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ut nunc quis justo soda",
+            "date":"22 Oct 2020",
+            "content":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident."
+        },
+        {
+            "id":3,
+            "image":"https://picsum.photos/900",
+            "title":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ut nunc quis justo soda",
+            "date":"22 Oct 2020",
+            "content":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident."
+        },
+        {
+            "id":4,
+            "image":"https://picsum.photos/900",
+            "title":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ut nunc quis justo soda",
+            "date":"22 Oct 2020",
+            "content":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident."
+        },
+        {
+            "id":5,
+            "image":"https://picsum.photos/900",
+            "title":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ut nunc quis justo soda",
+            "date":"22 Oct 2020",
+            "content":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident."
+        },
+        {
+            "id":6,
+            "image":"https://picsum.photos/900",
+            "title":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ut nunc quis justo soda",
+            "date":"22 Oct 2020",
+            "content":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident."
+        }
+    ]
     return (
         // All Pages
         <div className="min-h-screen">
@@ -24,51 +68,33 @@ const Home = () => {
                 <div className="w-full">
                 <div className="lg:grid lg:grid-cols-3">
                     {/* Card */}
-                    <div className="h-80 mx-4 mt-20 lg:my-2 group">
-                        <Link to="/article">
-                        {/* {hover && ( */}
-                            <div className="h-4/5">
-                                <img src="https://picsum.photos/900"  className="object-cover h-full w-full" alt="screen2"/>
+                    {articles.map((article) => 
+                        (
+                            <div className="h-80 mx-4 mt-20 lg:my-2 group" onMouseEnter={() => setHover({activeIndex: article.id})} onMouseLeave={() => setHover({activeIndex: 0})}>
+                                <Link to="/article">
+                                {hover.activeIndex != article.id&&(
+                                    <div className="h-4/5">
+                                        <img src={article.image}  className="object-cover h-full w-full" alt="screen2"/>
+                                    </div>
+                                )}
+                                <div>
+                                    <p className="text-xs mt-2 text-gray-400">{article.date}</p>
+                                </div>
+                                <div className="text-sm font-semibold mt-2 group-hover:text-yellow-600">
+                                    {article.title}
+                                </div>
+                                {hover.activeIndex == article.id&& (
+                                    <div className="mt-2">
+                                        <p className="text-sm">{article.content}</p>
+                                        <p className="text-sm text-yellow-600 mt-2">Read more . . .</p>
+                                    </div>
+                                )}
+                                </Link>
                             </div>
-                        {/* )} */}
-                        <div>
-                            <p className="text-xs mt-2 text-gray-400">22 Oct 2020</p>
-                        </div>
-                        <div className="text-sm font-semibold mt-2 group-hover:text-yellow-600">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ut nunc quis justo soda
-                        </div>
-                        {/* {!hover && (
-                            <div className="mt-2">
-                                <p className="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.</p>
-                                <p className="text-sm text-yellow-600 mt-2">Read more . . .</p>
-                            </div>
-                        )} */}
-                        </Link>
-                    </div>
+                        )
+                    )}
 
-                    <div className="h-80 mx-4 mt-20 lg:my-2 group">
-                        <Link to="/article">
-                        {/* {hover && ( */}
-                            <div className="h-4/5">
-                                <img src="https://picsum.photos/900"  className="object-cover h-full w-full" alt="screen2"/>
-                            </div>
-                        {/* )} */}
-                        <div>
-                            <p className="text-xs mt-2 text-gray-400">22 Oct 2020</p>
-                        </div>
-                        <div className="text-sm font-semibold mt-2 group-hover:text-yellow-600">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ut nunc quis justo soda
-                        </div>
-                        {/* {!hover && (
-                            <div className="mt-2">
-                                <p className="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.</p>
-                                <p className="text-sm text-yellow-600 mt-2">Read more . . .</p>
-                            </div>
-                        )} */}
-                        </Link>
-                    </div>
-
-                    <div className="h-80 mx-4 mt-20 lg:my-2 group" onMouseEnter={() => setHover(false)} onMouseLeave={() => setHover(true)}>
+                    {/* <div className="h-80 mx-4 mt-20 lg:my-2 group" onMouseEnter={() => setHover(false)} onMouseLeave={() => setHover(true)}>
                         <Link to="/article">
                         {hover && (
                             <div className="h-4/5">
@@ -88,70 +114,8 @@ const Home = () => {
                             </div>
                         )}
                         </Link>
-                    </div>
-                    <div className="h-80 mx-4 mt-20 lg:my-2 group">
-                        <Link to="/article">
-                        {/* {hover && ( */}
-                            <div className="h-4/5">
-                                <img src="https://picsum.photos/900"  className="object-cover h-full w-full" alt="screen2"/>
-                            </div>
-                        {/* )} */}
-                        <div>
-                            <p className="text-xs mt-2 text-gray-400">22 Oct 2020</p>
-                        </div>
-                        <div className="text-sm font-semibold mt-2 group-hover:text-yellow-600">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ut nunc quis justo soda
-                        </div>
-                        {/* {!hover && (
-                            <div className="mt-2">
-                                <p className="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.</p>
-                                <p className="text-sm text-yellow-600 mt-2">Read more . . .</p>
-                            </div>
-                        )} */}
-                        </Link>
-                    </div>
-                    <div className="h-80 mx-4 mt-20 lg:my-2 group">
-                        <Link to="/article">
-                        {/* {hover && ( */}
-                            <div className="h-4/5">
-                                <img src="https://picsum.photos/900"  className="object-cover h-full w-full" alt="screen2"/>
-                            </div>
-                        {/* )} */}
-                        <div>
-                            <p className="text-xs mt-2 text-gray-400">22 Oct 2020</p>
-                        </div>
-                        <div className="text-sm font-semibold mt-2 group-hover:text-yellow-600">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ut nunc quis justo soda
-                        </div>
-                        {/* {!hover && (
-                            <div className="mt-2">
-                                <p className="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.</p>
-                                <p className="text-sm text-yellow-600 mt-2">Read more . . .</p>
-                            </div>
-                        )} */}
-                        </Link>
-                    </div>
-                    <div className="h-80 mx-4 mt-20 lg:my-2 group">
-                        <Link to="/article">
-                        {/* {hover && ( */}
-                            <div className="h-4/5">
-                                <img src="https://picsum.photos/900"  className="object-cover h-full w-full" alt="screen2"/>
-                            </div>
-                        {/* )} */}
-                        <div>
-                            <p className="text-xs mt-2 text-gray-400">22 Oct 2020</p>
-                        </div>
-                        <div className="text-sm font-semibold mt-2 group-hover:text-yellow-600">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ut nunc quis justo soda
-                        </div>
-                        {/* {!hover && (
-                            <div className="mt-2">
-                                <p className="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.</p>
-                                <p className="text-sm text-yellow-600 mt-2">Read more . . .</p>
-                            </div>
-                        )} */}
-                        </Link>
-                    </div>
+                    </div> */}
+                   
                 </div>
                 </div>
                 <div className="invisible lg:visible w-0 lg:w-32">
